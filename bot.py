1018d612589198b28aa5de88bd3ed6cfc87bc98e
Alexandre Antrieux <alexandre.antrieux@epitech.eu>
2022-07-12 15:44:55 +0200

    Add color "yellow" while process and "green" when finish

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
 import math
+import colorama
 
 url = "http://localhost/views/hashtag.php"                          # url to post request
 hash = input("Enter the hashtag (without #): ")                     # get hashtag from user
@@ -7,10 +8,13 @@ nbr = input("how many hashtag do you want ? ")                      # get number
 nbr = int(nbr)                                                      # convert nbr to int
 
 
-def progress_bar(progress, total):                                  # progress bar function
+def progress_bar(progress, total, color=colorama.Fore.YELLOW):      # progress bar function with Yellow font color
     percent = (progress/total)*100                                  # calculate percentage
     bar = '█' * int(percent) + '-' * int(100-percent)               # create progress bar
-    print ('\r', bar, '{:.2f}%'.format(percent), end='')            # print progress bar
+    print (color + f'\r', bar, '{:.2f}%'.format(percent), end='')   # print progress bar with color
+    if progress == total:                                           # if progress is equal to total
+        print(colorama.Fore.GREEN + 
+        f'\r', bar, '{:.2f}%'.format(percent), end='')              #then print progress bar with green color                     
 
 
 def create_Hashtag():                                               # create hashtag function
@@ -25,3 +29,4 @@ def create_Hashtag():                                               # create has
 
 
 create_Hashtag()
+print(colorama.Fore.RESET)
